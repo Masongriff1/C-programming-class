364ea3a682c0b57e5e0e6fb6cfb9d4991e724d57
TechProblem <masongriffith22@gmail.com>
2024-06-12 11:25:24 -0700

    edited 1.1

--- 1.1 assignment/1.1 assignment/1.1 assignment.cpp
+++ 1.1 assignment/1.1 assignment/1.1 assignment.cpp
@@ -10,8 +10,58 @@
 using namespace std;
 int main() // main function
 {
+	const int rows = 3;
+	const int cols = 3;
+	int myArray[rows][cols];
+
+	cout << "Enter elements for this 3*3 array: " << endl;
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+			cout << "Please enter an element: ";
+			cin >> myArray[i][j];
+		}
+	}
+	cout << "Elements of the 3*3 array: " << endl;
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j){
+			cout << myArray[i][j];
+		}
+	}
+	
+	
+	
+	/*string str1 = "This is C language";
+	string str2 = "java language";
+	cout << "Before replacement, string is: " << str1 << '\n';
+	str1.replace(8, 1, str2,0,4);
+	cout << "After replacement, string is: " << str1 << '\n';
+	return 0;*/
+	
+	
 	//declare and initialize a string
-	string myString = "Hello, welcome to UAT.";
+	/*string myString = "Hello, welcome to UAT.";
+
+	cout << "Original String: " << myString << endl;
+
+	myString.append(" Enjoy your visit.");
+	cout << "Original String after append: " << myString << endl;
+
+	string subString = myString.substr(7, 7);
+	cout << "Substring extracted using substr(): " << subString << endl;
+
+	string anotherString = "Hello, welcome to UAT.";
+	int result = myString.compare(anotherString);
+	if (result == 0) {
+		cout << "The strings are equal" << endl;
+	}
+	else if (result < 0) {
+		cout << "myString is less than anotherString." << endl;
+	}
+	else {
+		cout << "myString is greater than anotherString." << endl;
+	}*/
+
+	/*string myString = "Hello, welcome to UAT.";
 
 	cout << "Original string: " << myString << endl;
 
@@ -20,7 +70,12 @@ int main() // main function
 	
 	string substring = "welcome";
 	int position = myString.find(substring);
-		if (position < myString.size()) {
+		
+	
+	*/
+	
+	
+	/*if (position < myString.size()) {
 			cout << "Substring " << substring << " Found at position " << position << endl;
 	}
 
@@ -36,7 +91,7 @@ int main() // main function
 		}
 		else {
 			cout << "The string is not empty" << endl;
-		}
+		}*/
 
 	
 	/*char choice;
